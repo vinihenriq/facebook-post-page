49f20acb669f833b9ed6c7413f7075612c56e428
vinihenriq <viniciusilva.henriq@gmail.com>
2021-09-05 22:16:42 -0300

    commitando projeto

---
+++ src/components/header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "../App.css";
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <img src="https://i.imgur.com/KDIDiSE.png" />
+        <div>
+          <span>Meu perfil</span>
+          <i className="material-icons">account_circle</i>
+        </div>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
